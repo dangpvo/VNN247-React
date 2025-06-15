f9c495493b1002b2a763e7a7272bbee3a4eca92e
Dennis Vo <dennisphuc@gmail.com>
2025-06-15 10:41:25 +0200

    feat: finish basic version of hero section in home page

--- src/pages/home/sections/card/Card.jsx
+++ src/pages/home/sections/card/Card.jsx
@@ -10,6 +10,15 @@ const Card = ({
         <div className="img">
           <img src={cover} alt="" />
         </div>
+        <div className="text">
+          <span className="category">{category}</span>
+          <h1 className="titleBg">{title}</h1>
+
+          <div className="author flex">
+            <span>by {authorName}</span>
+            <span>{time}</span>
+          </div>
+        </div>
       </div>
     </>
   );
