b8693acc2431fa068948c87be30be9da70402cf8
Dennis Vo <dennisphuc@gmail.com>
2025-07-08 20:31:28 +0200

    feat: finish UI + responsive for LatestNews section

--- src/pages/home/sections/latestNews/LatestNews.jsx
+++ src/pages/home/sections/latestNews/LatestNews.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LatestNews.css";
+import { hero } from "../../../../assets/assets";
 
 const LatestNews = () => {
+  const [items, setItems] = useState(hero);
+
   return (
     <>
-      <section className="container">LatestNews</section>
+      {/* <div>{items[1].title}</div> */}
+      <section className="latestnews container">
+        <div className="latestnews-left">
+          <div className="latestnews-left__title">{items[0].title}</div>
+          <div className="latestnews-left__details">
+            <span className="latestnews__cat">{items[0].category + " "}</span> |
+            <span className="latestnews__src"> VnExpress</span>
+          </div>
+          <div className="latestnews-left__img">
+            <img src={items[0].cover} alt="" className="latestnews__img" />
+          </div>
+        </div>
+        <div className="latestnews-right">
+          <div className="news-card-horizontal-1">
+            <div className="news-card-hori-1__left">
+              <div className="news-card-hori-1__title">{items[1].title}</div>
+              <div className="news-card-hori-1__desc">
+                {items[1].desc[0].para1.slice(0, 70)}...
+              </div>
+              <div className="news-card-hori-1__details">
+                <span className="latestnews__cat">
+                  {items[1].category + " "}
+                </span>
+                | <span className="latestnews__src">VnExpress</span>
+              </div>
+            </div>
+            <div className="news-card-hori-1__right">
+              <img src={items[1].cover} alt="" className="latestnews__img" />
+            </div>
+          </div>
+
+          <div className="news-card-horizontal-1">
+            <div className="news-card-hori-1__left">
+              <div className="news-card-hori-1__title">{items[2].title}</div>
+              <div className="news-card-hori-1__desc">
+                {items[2].desc[0].para1.slice(0, 70)}...
+              </div>
+              <div className="news-card-hori-1__details">
+                <span className="latestnews__cat">
+                  {items[2].category + " "}
+                </span>
+                | <span className="latestnews__src">VnExpress</span>
+              </div>
+            </div>
+            <div className="news-card-hori-1__right">
+              <img src={items[2].cover} alt="" className="latestnews__img" />
+            </div>
+          </div>
+
+          <div className="news-card-horizontal-1">
+            <div className="news-card-hori-1__left">
+              <div className="news-card-hori-1__title">{items[3].title}</div>
+              <div className="news-card-hori-1__desc">
+                {items[3].desc[0].para1.slice(0, 70)}...
+              </div>
+              <div className="news-card-hori-1__details">
+                <span className="latestnews__cat">
+                  {items[3].category + " "}
+                </span>
+                | <span className="latestnews__src">VnExpress</span>
+              </div>
+            </div>
+            <div className="news-card-hori-1__right">
+              <img src={items[3].cover} alt="" className="latestnews__img" />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
